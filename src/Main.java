157513b5392b7b6e39a91142e400ebc8ac1494cc
Nikita Sachik <nikitasachik@gmail.com>
2023-03-05 01:56:34 +0200

    If user win bet

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,15 @@ import org.w3c.dom.ls.LSOutput;
 import java.util.Locale;
 
 public class Main {
-
-    public static double getPercentOfEmployedStudent(Student[] studentsInfo) {
-        double percent;
-        double employers = 0;
-        double correctAge = 0;
-        for (int i = 0; i < studentsInfo.length; i++) {
-            if (studentsInfo[i].age >= 30 && studentsInfo[i].age <= 40) {
-                correctAge++;
-            }
-            if (studentsInfo[i].isEmployed) {
-                employers++;
-            }
-        }
-
-        //percent = (100 / (correctAge / employers)) * 0.01;
-        percent = correctAge / employers;
-        return percent;
-        // Write code above this line
-    }
-
     public static void main(String[] args) {
-        System.out.println("Hello!!!");
+        System.out.println(resultOfBet(3,3,3,3));
     }
 
-
-
+    public static int resultOfBet(int num1, int num2, int num3, int num4)
+    {
+        int result = (num1 == num3 && num2 == num4) ?  2 : 0;
+        return result;
+    }
 }
 
 
