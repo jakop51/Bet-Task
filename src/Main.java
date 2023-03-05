ae5d7b6da674bc48cc90014724d86520651f964e
Nikita Sachik <nikitasachik@gmail.com>
2023-03-05 03:18:17 +0200

    return 2 1 0

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,15 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(resultOfBet(3,3,3,3));
+        System.out.println(resultOfBet(13,2,14,9));
     }
 
     public static int resultOfBet(int num1, int num2, int num3, int num4)
     {
         int result = (num1 == num3 && num2 == num4) ?  2 : 0;
+        if (result == 2)
+            return result;
+        result = (num1 <= num3 && num2 <= num4 ) ?  1 : 0;
         return result;
     }
 }
