20b90aaf64e64f4ead3d88002446ee16f14ba42c
Nikita Sachik <nikitasachik@gmail.com>
2023-03-07 06:50:45 +0200

    final result

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,12 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(resultOfBet(0,0, 1,3));
+        System.out.println(matchResult(0,0, 1,3));
     }
 
-    public static int resultOfBet(int num1, int num2, int num3, int num4)
-    {
-        int result = (num1 > num2 && num3 < num4  || num1 < num2 && num3 > num4) ? 0 : ((num1 == num3 && num2 == num4) ? 2 : 1);
-        return result;
+    public static int matchResult(int result1, int result2, int bet1, int bet2) {
+        return (result1 > result2 && bet1 < bet2  || result1 < result2 && bet1 > bet2 || result1 == result2 && bet1 != bet2 || result1 != result2 && bet1 == bet2)
+                ? 0 : ((result1 == bet1 && result2 == bet2) ? 2 : 1);
     }
 }
 
