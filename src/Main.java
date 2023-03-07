b839a4f5e8b0f2dedfdceff3b36526ea633308a3
Nikita Sachik <nikitasachik@gmail.com>
2023-03-07 05:33:33 +0200

    Think about logic, you can write a > b ? (c > d ? 1 : 2) : 3

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,28 @@ public class Main {
         System.out.println(5 > 7 ? 55 : 66);
         // Can create ((a == b ? 2 : 0) == 0) ? 0 : 1)
     }
+
+    public static int resultOfBet(int num1, int num2, int num3, int num4)
+    {
+        int result = (num1 == num3 && num2 == num4) ?  2 : 0;
+        if (result == 2)
+            return result;
+        result = (num1 <= num3 && num2 <= num4 ) ?  1 : 0;
+
+
+
+        //int i = ((num1 == num3 && num2 == num4) ? 2 : 0) > ((num1 <= num3 && num2 <= num4) ? 1 : 3) ? 1 : ((num1 == num3 && num2 == num4) ? 2 : 0);
+        return result;
+
+
+        //
+
+
+
+
+    }
+
+
 }
 
 
