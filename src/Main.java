b0b1ac445dd387869a34512e22fa9ea6e126b00f
Nikita Sachik <nikitasachik@gmail.com>
2023-03-07 05:03:51 +0200

    Idea to solve the exercize

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(5 > 7 ? 55 : 66);
+        // Can create ((a == b ? 2 : 0) == 0) ? 0 : 1)
     }
 }
 
