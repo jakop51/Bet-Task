0623e02b472a7e116a9bb2e5fe2a18a8f8439ad8
Nikita Sachik <nikitasachik@gmail.com>
2023-03-07 06:25:21 +0200

    Need final test

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,14 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(5 > 7 ? 55 : 66);
-        // Can create ((a == b ? 2 : 0) == 0) ? 0 : 1)
+        System.out.println(resultOfBet(0,0, 1,3));
     }
 
     public static int resultOfBet(int num1, int num2, int num3, int num4)
     {
-        int result = (num1 == num3 && num2 == num4) ?  2 : 0;
-        if (result == 2)
-            return result;
-        result = (num1 <= num3 && num2 <= num4 ) ?  1 : 0;
-
-
-
-        //int i = ((num1 == num3 && num2 == num4) ? 2 : 0) > ((num1 <= num3 && num2 <= num4) ? 1 : 3) ? 1 : ((num1 == num3 && num2 == num4) ? 2 : 0);
+        int result = (num1 > num2 && num3 < num4  || num1 < num2 && num3 > num4) ? 0 : ((num1 == num3 && num2 == num4) ? 2 : 1);
         return result;
-
-
-        //
-
-
-
-
     }
-
-
 }
 
 
